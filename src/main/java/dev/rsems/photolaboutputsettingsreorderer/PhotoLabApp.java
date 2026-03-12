b9c160f849dcbfc2889842eec07926ed9f54ce51
rob <rs@rsems.de>
2026-03-12 01:53:53 +0100

    Icon added

--- src/main/java/dev/rsems/photolaboutputsettingsreorderer/PhotoLabApp.java
+++ src/main/java/dev/rsems/photolaboutputsettingsreorderer/PhotoLabApp.java
@@ -3,9 +3,11 @@ package dev.rsems.photolaboutputsettingsreorderer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class PhotoLabApp extends Application {
 
@@ -18,6 +20,10 @@ public class PhotoLabApp extends Application {
         Scene scene = new Scene(loader.load());
         stage.setTitle("PhotoLab Output Settings Reorderer " + VERSION);
         stage.setScene(scene);
+        try {
+            stage.getIcons().add(new Image(
+                    Objects.requireNonNull(PhotoLabApp.class.getResourceAsStream("icon.ico"))));
+        } catch (Exception ignored) {}
         stage.show();
     }
 }
